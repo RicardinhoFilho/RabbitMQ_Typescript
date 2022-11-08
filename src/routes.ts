f66f5ab7fa2f5a353a077de025eed8e8cea4d0dd
Ricardo <ricardinhogiasson16@gmail.com>
2022-11-08 16:47:52 -0300

    Atualizando posto

--- src/routes.ts
+++ src/routes.ts
@@ -2,24 +2,27 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get(`/`, (req, res) => {
-  return res.status(200).json({
-    data: [
-      { id: "1", nome: "Ricardo" },
-      { id: "2", nome: "Sandro" },
-      { id: "3", nome: "Márcia" },
-      { id: "5", nome: "Ricardo Filho" },
-    ],
-    status: true,
-    message: null,
-  });
-});
+
 
 router.post(`/`, (req, res) => {
     return res.status(200).json({
       data: req.body,
       status: true,
       message: "Dados recebidos com sucesso!",
+    });
+  });
+
+
+  router.get(`/`, (req, res) => {
+    return res.status(200).json({
+      data: [
+        { id: "1", nome: "Ricardo" },
+        { id: "2", nome: "Sandro" },
+        { id: "3", nome: "Márcia" },
+        { id: "5", nome: "Ricardo Filho" },
+      ],
+      status: true,
+      message: null,
     });
   });
 
