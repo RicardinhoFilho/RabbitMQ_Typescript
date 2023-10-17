bbd841f1846b30a73858536e044141a9faba68a0
Ricardo <ricardinhogiasson16@gmail.com>
2023-10-17 08:43:13 -0300

    ADICIONANDO RABBIT MQ

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,28 @@
-import express from "express";
-import cors from "cors";
-import router from "./routes";
 
-const app = express();
+import client, { Connection, Channel, ConsumeMessage } from "amqplib";
+function sendMessages(channel: Channel): void {
+    for (let index = 0; index < 10; index++) {
+        const teste_const = { id: index, titulo: `Mensagem ${index}` };
+        channel.sendToQueue("teste", Buffer.from(JSON.stringify(teste_const)))
 
-app.use(cors());
+    }
 
-app.use(express.json({ limit: "10000kb" }));
-app.use(
-  express.urlencoded({
-    extended: false,
-    limit: "10000kb",
-    parameterLimit: 1000000,
-  })
-);
+}
 
-app.use(router);
+const consumer = (channel: Channel) => (msg: ConsumeMessage | null): void => {
+    if (msg) {
+        console.log(JSON.parse(msg.content.toString()));
+        channel.ack(msg);
+    }
+}
 
-app.listen(10000, () => console.log("Server started at http://localhost:10000"));
+
+const execute = async () => {
+    const connection: Connection = await client.connect("amqp://guest:guest@localhost:5672");
+    const channel: Channel = await connection.createChannel();
+    await channel.assertQueue("teste");
+    sendMessages(channel);
+    await channel.consume('teste', consumer(channel))
+}
+
+execute()
